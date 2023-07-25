9f8b897d8d7b12a4db6b67ce066f9a18e4c12d77
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-07-25 17:23:10 +0700

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(JWT_SECRET);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new AuthError('Ошибка авторизации1'));
   }
@@ -12,8 +13,10 @@ module.exports = (req, res, next) => {
   let payload;
   console.log(JWT_SECRET);
   try {
+    console.log(JWT_SECRET);
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-jwt-token');
   } catch (err) {
+    console.log(JWT_SECRET);
     return next(new AuthError('Ошибка авторизации2'));
   }
   req.user = payload;
